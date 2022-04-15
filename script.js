282454b571ba8cda9e9c98d755b888d44f8b6277
Collin Whalen <collinw@ihspharmacy.com>
2022-04-15 11:49:51 -0500

    both api call functions are set up, next is testing, and makeing sure html renders, may not need the testObj

--- script.js
+++ script.js
@@ -3,83 +3,77 @@
 //that were created by that 'USER'. You have full freedom in how you accomplish
 //the above objectives. You also have full freedom as far as design is concerned
 
-
-//user obj mapped to a 
+//user obj mapped to a
 //table of user links
 //a link will get the vlaue of user and make an api call for posts
 //posts are rendered to dom
 
 //start with a test api
 
-
 const testObj = [
-    {
-        user: 'Collin',
-        posts: [
-            {
-            post: "Hello Mike"
-        },
-        {
-          post: "I like tea."
-      }
-  ]
-    },
-    {
-      user: 'Mike',
-      posts:[ {
-          post: "Hello Collin"
+  {
+    user: "Collin",
+    posts: [
+      {
+        post: "Hello Mike",
       },
       {
-          post: "I like coffee."
-      }
-    ]
-  }
+        post: "I like tea.",
+      },
+    ],
+  },
+  {
+    user: "Mike",
+    posts: [
+      {
+        post: "Hello Collin",
+      },
+      {
+        post: "I like coffee.",
+      },
+    ],
+  },
 ];
 
 function fetchJSON() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(response => {
-       
-
-        renderUserTable()
-    })
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((response) => {
+      let userName = response.name;
+      let id = response.id;
+      renderUserTable(userName, id);
+    });
 }
 
+function renderUserTable(userName, id) {
+  for (var i = 0; i < 15 && id.length; i++) {
+    let table, row, cell;
 
-    
-function renderUserTable(users){
-    for (var i = 0; i < 15 && users.length; i++) {
-
-        let table, row, cell;
-        
-        table = document.createElement('table');
-        row = table.insertRow([i]); 
-        cell = row.insertCell([i]);
-        cell = document.createElement('a');
-        cell.innerHTML = $(this); //$(this).attr('value');
-        cell.setAttribute("class", "userCell");
-        cell.setAttribute("value", this);
-        document.getElementById("table-section").appendChild(table);
-    }
+    table = document.createElement("table");
+    row = table.insertRow([i]);
+    cell = row.insertCell([i]);
+    cell = document.createElement("a");
+    cell.innerHTML = userName; //$(this).attr('value');
+    cell.setAttribute("class", "userCell");
+    cell.setAttribute("value", id);
+    document.getElementById("table-section").appendChild(table);
+  }
 }
 
-
-
-function fetchUserPosts(x){
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(response => {
-        console.log('hello')
-
-    }
+function fetchUserPosts(userId) {
+  let id = String(userId);
+  fetch("https://jsonplaceholder.typicode.com/posts/" + id)
+    .then((response) => response.json())
+    .then((response) => {
+      console.log("hello");
+    });
 }
 
-$('call-api').onClick(function (){
-    fetchJSON()
-})
+$("call-api").onClick(function () {
+  fetchJSON();
+});
 
-$('.userCell').click(function () {
-    var user = $(this).attr('value');
-    fetchUserPosts(user);
-}  
+$(".userCell").click(function () {
+  var userId = $(this).attr("value");
+  fetchUserPosts(userId);
+});
